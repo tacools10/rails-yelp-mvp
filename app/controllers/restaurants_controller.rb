b7701684ecb51461781ce02be9b207c91300eedc
Thomas Cools <tacools10@gmail.com>
2016-11-23 16:54:14 +0100

    Further progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,7 +25,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :rating)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
 end
